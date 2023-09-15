6fc8459778e9d2d1be1565f1c889f3ece0d4094c
Anirudh Atole <anirudhatole@gmail.com>
2023-09-15 18:12:15 +0530

    Made a header using react

--- app.js
+++ app.js
@@ -3,7 +3,64 @@ import ReactDOM from "react-dom/client";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const h1 = React.createElement("h1",{
-    id:"header",
-},"Hello world");
-root.render(h1);
+
+const Logo  = () =>{
+    return(
+        <img style={
+            {
+                width:"30px",
+                marginLeft : "20px",
+                marginTop : "20px"
+            }
+        }
+        src="https://cdn-icons-png.flaticon.com/512/7799/7799536.png" alt="image"></img>
+    );
+}
+
+const UserIcon  = () =>{
+    return(
+        <img 
+        style={{
+            width : "20px",
+            marginLeft : "2px",
+        }
+        }
+        src="https://cdn-icons-png.flaticon.com/128/456/456212.png" alt="image"></img>
+    );
+}
+
+const Searchbar = () =>{
+    return(
+        <input
+        style={
+            {
+                width:"350px",
+                height:"30px",
+                margin:"20px"
+            }
+        }></input>
+    )
+}
+const HeaderComponent = ()=>{
+    return (<div style={
+        {backgroundColor: 'blue',
+        width : "500px"
+    }}>
+        <Logo />
+        <Searchbar />
+        <UserIcon />
+    </div>
+    )
+}
+// const NameA = ()=> <h1>Namaste React</h1>
+// const HeadElement = ()=>{
+//     return(
+//         <div>
+//             <NameA />
+//             <NameA />
+//         </div>
+//     );
+// }
+
+
+root.render(<HeaderComponent/>);
